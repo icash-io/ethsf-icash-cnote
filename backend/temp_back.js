a8473dd8bb4091013a8098a0d453243cff8e7370
Kent <khmakishima@ucdavis.edu>
2018-10-06 19:28:08 -0700

    changes

--- backend/temp_back.js
+++ backend/temp_back.js
@@ -12,7 +12,40 @@ const transactionObject = {
 };
 
 
-function ($scope, $stateParams) {
-	
+// Functions/API that we need. 
 
-}])
+
+// Bloom API: Binary check if there is an associated address with account.  Web3
+// https://bloom.co/docs/contracts/accounts#address-belongs-to-account
+
+accountRegistry = AccountRegistry.at("[address of registry contract]")
+hasBloomId = accountRegistry.addressBelongsToAccount.call(address)
+
+
+// DHARMA API 
+// https://developer.dharma.io/tutorials/dharma-js
+
+
+import { Dharma } from "@dharmaprotocol/dharma.js";
+const host = "http://localhost:8545";
+
+const provider = window.web3.currentProvider;
+const dharma = new Dharma(provider);
+
+// RCN API
+// https://github.com/ripio/rcn-network
+
+
+function createLoan(
+    address _oracle,
+    address _borrower,
+    bytes32 _currency,
+    uint256 _amount,
+    uint256 _interestRate,
+    uint256 _penaltyInterestRate,
+    uint256 _duesIn,
+    uint256 _firstPayment,
+    uint256 _expiration,
+    string _metadata
+) public returns (uint256 id) {
+
