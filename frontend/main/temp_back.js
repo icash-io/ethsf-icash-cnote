014fe4c492ce7495fce627317b808d3d837eb868
Kent <khmakishima@ucdavis.edu>
2018-10-07 06:53:46 -0700

    dharma adding

--- frontend/main/temp_back.js
+++ frontend/main/temp_back.js
@@ -1,5 +1,23 @@
 // Setup Stuff 
+// const Web3 = require('web3');
+// const Dharma = require('@dharmaprotocol/dharma.js');
+// const ABIDecoder = require('abi-decoder');
+// const promisify = require('tiny-promisify');
+// const BigNumber = require('bignumber.js');
+// const compact = require('lodash.compact');
+// const request = require('request-json');
+// const client = request.createClient('http://dharma-relayer-prod-993135204.us-east-1.elb.amazonaws.com/');
 
+// const DebtRegistry = require('./build/contracts/DebtRegistry.json')
+// const DebtKernel = require('./build/contracts/DebtKernel.json')
+// const RepaymentRouter = require('./build/contracts/RepaymentRouter.json')
+// const TokenTransferProxy = require('./build/contracts/TokenTransferProxy.json')
+// const TokenRegistry = require('./build/contracts/TokenRegistry.json')
+// const DebtToken = require('./build/contracts/DebtToken.json')
+// const SimpleInterestTermsContract = require('./build/contracts/SimpleInterestTermsContract.json')
+
+// ABIDecoder.addABI(DebtKernel.abi);
+// ABIDecoder.addABI(RepaymentRouter.abi);
 // mapping(address => uint256) public accountByAddress;
 
 const provider = window.web3.currentProvider;
@@ -71,8 +89,39 @@ function loancreate(){
 // const host = "http://localhost:8545";
 
 function loanaccept(){
-	
+
 }
+
+function instantiateDharma() {
+    const networkId = await promisify(web3.version.getNetwork)();
+    const accounts = await promisify(web3.eth.getAccounts)();
+    if (!accounts.length) {
+        throw new Error('ETH account not available');
+    }
+    defaultAccount = accounts[0];
+
+    if (!(networkId in DebtKernel.networks &&
+        networkId in RepaymentRouter.networks &&
+        networkId in TokenTransferProxy.networks &&
+        networkId in TokenRegistry.networks &&
+        networkId in DebtToken.networks &&
+        networkId in SimpleInterestTermsContract.networks &&
+        networkId in DebtRegistry.networks)) {
+        throw new Error('Unable to connect to the blockchain');
+    }
+
+    const dharmaConfig = {
+        kernelAddress: DebtKernel.networks[networkId].address,
+        repaymentRouterAddress: RepaymentRouter.networks[networkId].address,
+        tokenTransferProxyAddress: TokenTransferProxy.networks[networkId].address,
+        tokenRegistryAddress: TokenRegistry.networks[networkId].address,
+        debtTokenAddress: DebtToken.networks[networkId].address,
+        simpleInterestTermsContractAddress: SimpleInterestTermsContract.networks[networkId].address,
+        debtRegistryAddress: DebtRegistry.networks[networkId].address
+    };
+
+    return new Dharma.default(web3.currentProvider, dharmaConfig);
+}`
 
 // RCN API
 // https://github.com/ripio/rcn-network
